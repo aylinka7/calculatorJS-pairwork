dc57342fd039f4a9f5e975b9a3c20b895d2da10d
aylinka7 <ayladylova@gmail.com>
2025-11-07 22:58:27 +0600

    evaluateRPN is done

--- app.js
+++ app.js
@@ -40,7 +40,25 @@ function convert(tokens) {
             stack.pop();
         }
     }
-
     while (stack.length) output.push(stack.pop());
     return output;
+}
+
+function evaluateRPN(rpn) {
+    const stack = [];
+    for (const token of rpn) {
+        if (!isNaN(token)) {
+            stack.push(parseFloat(token));
+        } else {
+            const b = stack.pop();
+            const a = stack.pop();
+            switch (token) {
+                case "+": stack.push(a + b); break;
+                case "-": stack.push(a - b); break;
+                case "*": stack.push(a * b); break;
+                case "/": stack.push(b === 0 ? NaN : a / b); break;
+            }
+        }
+    }
+    return stack[0];
 }
