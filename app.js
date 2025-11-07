8424f1517a8c56f61a2a1e876f961a5377ffd60b
aylinka7 <ayladylova@gmail.com>
2025-11-07 22:46:51 +0600

    method convert is done

--- app.js
+++ app.js
@@ -5,16 +5,42 @@ const ops = {
     "/": (a, b) => b === 0 ? NaN : a / b
 };
 
-function createCalculator() {
-    return {
-        current: "0",
-        operand: null,
-        operator: null,
-        history: [],
-        inputDigit(d) { /* ... */ },
-        chooseOperator(op) { /* ... */ },
-        evaluate() { /* ... */ },
-        clear() { this.current = "0"; },
-        allClear() { Object.assign(this, createCalculator()); }
-    };
+const precedence = {
+    "+": 1,
+    "-": 1,
+    "*": 2,
+    "/": 2
+};
+function isOperator(ch) {
+    return ["+", "-", "*", "/"].includes(ch);
+}
+
+function convert(tokens) {
+    const output = [];
+    const stack = [];
+
+    for (const token of tokens) {
+        if (!isNaN(token)) {
+            output.push(token);
+        } else if (isOperator(token)) {
+            while (
+                stack.length &&
+                isOperator(stack.at(-1)) &&
+                precedence[stack.at(-1)] >= precedence[token]
+                ) {
+                output.push(stack.pop());
+            }
+            stack.push(token);
+        } else if (token === "(") {
+            stack.push(token);
+        } else if (token === ")") {
+            while (stack.length && stack.at(-1) !== "(") {
+                output.push(stack.pop());
+            }
+            stack.pop();
+        }
+    }
+
+    while (stack.length) output.push(stack.pop());
+    return output;
 }
